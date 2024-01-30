b9cce47b083ef1c51f7b95f0367ba9dbb623b767
Narotam-Mishra <narotam21jan@gmail.com>
2024-01-30 23:29:31 +0530

    react-responsive-dashboard_1.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,56 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import { RouterProvider, createBrowserRouter, Outlet } from "react-router-dom"
 import Home from "./pages/home/Home"
 import Users from "./pages/users/Users"
 import Products from "./pages/products/Products";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Menu from "./components/menu/Menu";
+import Login from "./pages/login/Login";
+import './styles/global.scss';
 
 function App() {
+  const Layout = () => {
+    return(
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    )
+  }
 
   const router = createBrowserRouter([
     {
       path: "/",
       element: (
-        <Home />
+        <Layout />
       ),
+      children:[
+        {
+          path: "/",
+          element: <Home/>
+        },
+        {
+          path: "/users",
+          element: <Users />
+        },
+        {
+          path: "/products",
+          element: <Products />
+        },
+      ]
     },
     {
-      path: "users",
-      element: <Users />,
-    },
-    {
-      path: "products",
-      element: <Products />,
-    },
+      path: "/login",
+      element: <Login />
+    }
   ]);
 
   return(
