e96580a7c0fd34722f8b6dbd5e8933dafa11f72c
Narotam-Mishra <narotam21jan@gmail.com>
2024-02-01 12:16:14 +0530

    react-responsive-dashboard_1.5

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,33 +1,23 @@
 import { Link } from 'react-router-dom';
 import './Menu.scss';
+import { menu } from '../../data'
 
 const Menu = () => {
   return (
-    <div className='menu'>
-      <div className="item">
-        <span className="title">MAIN</span>
-        <Link to="/" className='listItem'>
-            <img src="/home.svg" alt="Home icon" />
-            <span className="listItemTitle">Home</span>
-        </Link>
-        <Link to="/" className='listItem'>
-            <img src="/profile.svg" alt="Profile icon" />
-            <span className="listItemTitle">Profile</span>
-        </Link>
-      </div>
-      <div className="item">
-        <span className="title">MAIN</span>
-        <Link to="/" className='listItem'>
-            <img src="/home.svg" alt="Home icon" />
-            <span className="listItemTitle">Home</span>
-        </Link>
-        <Link to="/" className='listItem'>
-            <img src="/profile.svg" alt="Profile icon" />
-            <span className="listItemTitle">Profile</span>
-        </Link>
-      </div>
+    <div className="menu">
+      {menu.map((item) => (
+        <div className="item" key={item.id}>
+          <span className="title">{item.title}</span>
+          {item.listItems.map((listItem) => (
+            <Link to="/" className="listItem" key={listItem.id}>
+              <img src={listItem.icon} alt="Home icon" />
+              <span className="listItemTitle">{listItem.title}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
 export default Menu
