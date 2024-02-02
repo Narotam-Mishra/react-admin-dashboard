b7b7813f8f1783bfa31a1890e8b5429abf3b2bad
Narotam-Mishra <narotam21jan@gmail.com>
2024-02-03 00:11:42 +0530

    react-responsive-dashboard_1.8

--- src/components/dataTable/DataTable.tsx
+++ src/components/dataTable/DataTable.tsx
@@ -4,40 +4,69 @@ import './DataTable.scss';
 const DataTable = () => {
 
     const columns: GridColDef[] = [
-        { field: 'id', headerName: 'ID', width: 90 },
-        {
-          field: 'firstName',
-          headerName: 'First name',
-          width: 150,
-          editable: true,
+      { field: "id", headerName: "ID", width: 90 },
+      {
+        field: "avatar",
+        headerName: "Avatar",
+        width: 100,
+        renderCell: (params) => {
+          return (
+            <img src={params.row.img || "/noavatar.png"} alt="avatar icon" />
+          );
         },
-        {
-          field: 'lastName',
-          headerName: 'Last name',
-          width: 150,
-          editable: true,
+      },
+      {
+        field: "actions",
+        headerName: "Actions",
+        width: 100,
+        renderCell: (params) => {
+          return (
+            <div className="action">
+                <div className="view">View</div>
+                <div className="delete">Delete</div>
+            </div>
+          );
         },
-        {
-          field: 'age',
-          headerName: 'Age',
-          type: 'number',
-          width: 110,
-          editable: true,
-        },
-        {
-          field: 'fullName',
-          headerName: 'Full name',
-          description: 'This column has a value getter and is not sortable.',
-          sortable: false,
-          width: 160,
-          valueGetter: (params: GridValueGetterParams) =>
-            `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-        },
-      ];
+      },
+      {
+        field: "status",
+        headerName: "Status",
+        width: 100,
+        type: "boolean",
+      },
+      {
+        field: "firstName",
+        headerName: "First name",
+        width: 150,
+        editable: true,
+      },
+      {
+        field: "lastName",
+        headerName: "Last name",
+        width: 150,
+        editable: true,
+      },
+      {
+        field: "age",
+        headerName: "Age",
+        type: "number",
+        width: 110,
+        editable: true,
+      },
+      {
+        field: "fullName",
+        headerName: "Full name",
+        description: "This column has a value getter and is not sortable.",
+        sortable: false,
+        width: 160,
+        valueGetter: (params: GridValueGetterParams) =>
+          `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+      },
+    ];
       
       const rows = [
-        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
-        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
+        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14, status: true },
+        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31, status: true },
         { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
         { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
         { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
