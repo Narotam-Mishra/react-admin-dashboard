770f7a4e6c707779afe57511e5746123b5eb5bc1
Narotam-Mishra <narotam21jan@gmail.com>
2024-02-02 13:26:42 +0530

    react-responsive-dashboard_1.6

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,8 @@
+import BarChartBox from '../../components/barChartBox/BarChartBox'
+import ChartBox from '../../components/chartBox/ChartBox'
+import PieChartBox from '../../components/pieChartBox/PieChartBox'
 import TopBox from '../../components/topBox/TopBox'
+import { barChartBoxRevenue, barChartBoxVisit, chartBoxConversion, chartBoxProduct, chartBoxRevenue, chartBoxUser } from '../../data'
 import './Home.scss'
 
 const Home = () => {
@@ -7,14 +11,30 @@ const Home = () => {
       <div className="box box1">
         <TopBox />
       </div>
-      <div className="box box2">Box2</div>
-      <div className="box box3">Box3</div>
-      <div className="box box4">Box4</div>
-      <div className="box box5">Box5</div>
-      <div className="box box6">Box6</div>
-      <div className="box box7">Box7</div>
-      <div className="box box8">Box8</div>
-      <div className="box box9">Box9</div>
+      <div className="box box2">
+        <ChartBox {...chartBoxUser}/>
+      </div>
+      <div className="box box3">
+        <ChartBox {...chartBoxProduct }/>
+      </div>
+      <div className="box box4">
+        <PieChartBox />
+      </div>
+      <div className="box box5">
+        <ChartBox {...chartBoxConversion }/>
+      </div>
+      <div className="box box6">
+        <ChartBox {...chartBoxRevenue }/>
+      </div>
+      <div className="box box7">
+        
+      </div>
+      <div className="box box8">
+        <BarChartBox {...barChartBoxVisit}/>
+      </div>
+      <div className="box box9">
+        <BarChartBox {...barChartBoxRevenue}/>
+      </div>
     </div>
   )
 }
