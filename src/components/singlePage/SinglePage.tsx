b8400944591c6030330685c00e81f72dbc1e2ddb
Narotam-Mishra <narotam21jan@gmail.com>
2024-02-03 19:43:56 +0530

    react-responsive-dashboard_1.9

---
+++ src/components/singlePage/SinglePage.tsx
@@ -0,0 +1,9 @@
+import './SinglePage.scss'
+
+const SinglePage = () => {
+  return (
+    <div className='single'>SinglePage</div>
+  )
+}
+
+export default SinglePage
---
+++ src/pages/userCom/User.tsx
@@ -0,0 +1,9 @@
+import './User.scss'
+
+const User = () => {
+  return (
+    <div className='user'>User</div>
+  )
+}
+
+export default User
